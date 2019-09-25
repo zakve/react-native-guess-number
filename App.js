20c7ee23f38f5deb130fcafa3214a3f9f65a3ea3
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-09-25 15:00:50 +0200

    Add NativeBase

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// UI native-base
+import { Container, Text, Button } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <Container style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
