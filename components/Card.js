4e6124895f794989979e49e6640d022c20f20895
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-09-25 17:04:36 +0200

    Update Card component

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Image } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Form, Item, Input, Label } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Form, Item, Input, Label, View } from 'native-base';
 
 export default class CardShow extends React.Component {
     render() {
@@ -14,9 +14,29 @@ export default class CardShow extends React.Component {
                         <Item floatingLabel>
                             <Input />
                         </Item>
+                        <View style={styles.buttonContainer}>
+                            <Button transparent danger style={styles.button} onPress={() => { console.log("Reset!") }}>
+                                <Text>Reset</Text>
+                            </Button>
+                            <Button success style={styles.button} onPress={() => { console.log("Confirm") }}>
+                                <Text>Confirm</Text>
+                            </Button>
+                        </View>
+
                     </Body>
                 </CardItem>
             </Card>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: '80%',
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    button: {
+        width: 200
+    }
+})
