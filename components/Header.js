0722d7be0b417dd4a3a7e6b145d4134c16f5e02d
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-09-27 12:01:57 +0200

    Fix Header height

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import { StyleSheet } from "react-native";
-import { Platform, Container, Header, Left, Body, Right, Title } from 'native-base';
+import React from 'react';
+import { Header, Left, Body, Right, Title } from 'native-base';
 
 export default class HeaderTitle extends React.Component {
     render() {
         return (
-            <Container>
-                <Header>
-                    <Left />
-                    <Body>
-                        <Title>{this.props.title}</Title>
-                    </Body>
-                    <Right />
-                </Header>
-            </Container>
+            <Header>
+                <Left />
+                <Body>
+                    <Title>{this.props.title}</Title>
+                </Body>
+                <Right />
+            </Header>
         );
     }
 }
