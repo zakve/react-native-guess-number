5293974e23761021cde4b4d970643c68ebd542b0
zakve <zaklas.martin@gmail.com>
2019-11-27 13:20:06 +0100

    Refactor GameOverScreen to hooks

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,14 +2,10 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Alert } from 'react-native';
 import { Card, Text, Button, View } from 'native-base';
 
-export default class GameOverScreen extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Text>The Game is over!</Text>
-        )
-    }
+const GameOverScreen = props => {
+    return (
+        <Text>The Game is over!</Text>
+    )
 }
+
+export default GameOverScreen;
