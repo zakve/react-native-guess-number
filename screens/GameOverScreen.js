5b0316e3b2165c040f860d45e4086e83829bddb8
zakve <zaklas.martin@gmail.com>
2019-12-07 12:23:46 +0100

    Update GameOver screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Image } from 'react-native';
-import { Card, Text, Button, View } from 'native-base';
+import { Card, Text, Button, View, H1, } from 'native-base';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is over!</Text>
-            <Image source={require('../assets/img/success.png')} />
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
-            <Button onPress={props.onRestart}>
+            <H1>The Game is over!</H1>
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/img/success.png')} style={styles.image} />
+            </View>
+            <Text>Your phone needed {props.roundsNumber} rounds to guess the number {props.userNumber}.</Text>
+            <Button style={styles.btn} onPress={props.onRestart}>
                 <Text>Restart game</Text>
             </Button>
         </View>
@@ -22,6 +23,22 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         padding: 10,
         alignItems: 'center',
+    },
+    imageContainer: {
+        width: '80%',
+        height: 300,
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    btn: {
+        marginVertical: 10
     }
 })
 
